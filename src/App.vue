97a8afb6fab33be14353c5149a756272e9f240ef
Azim85 <avaz_53@icloud.com>
2022-08-31 01:05:16 +0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,87 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container" style="height: 80vh">
+    <div class="header">
+      <h1 class="text-center">Fargona academik litseyi</h1>
+      <p>Jami o'zbek tili yo'nalishidagi o'g'il bola talabalar: {{ data.length }} ta</p>
+    </div>
+    <div
+      class="table-responsive"
+      style="overflow-x: auto; overflow-y: auto; height: 100%"
+    >
+      <table class="table position-relative">
+        <thead>
+          <tr class="position-sticky text-white">
+            <th
+              scope="col"
+              class="position-sticky top-0 bg-dark border text-center"
+              v-for="h in head"
+              :key="h"
+            >
+              {{ h }}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(d, i) in data" :key="d.id_raqami" class="border">
+            <td>{{ i + 1 }}</td>
+            <td>{{ d.otm_nomi }}</td>
+            <td>{{ d.talim_shakli }}</td>
+            <td>{{ d.talim_yonalishi }}</td>
+            <td>{{ d.talim_tili }}</td>
+            <td>{{ d.FIO }}</td>
+            <td>{{ d.Jinsi }}</td>
+            <td>{{ d.id_raqami }}</td>
+            <td>{{ d.kitob_raqami }}</td>
+            <td>{{ d.fan1 }}</td>
+            <td>{{ d.FIELD11 }}</td>
+            <td>{{ d.FIELD12 }}</td>
+            <td>{{ d.Fan2 }}</td>
+            <td>{{ d.FIELD14 }}</td>
+            <td>{{ d.FIELD15 }}</td>
+            <td>{{ d.Fan3 }}</td>
+            <td>{{ d.FIELD17 }}</td>
+            <td>{{ d.FIELD18 }}</td>
+            <td>{{ d.umumiy_ball }}</td>
+            <td>{{ d.Sana }}</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import sortedArray from "@/data/students";
+import heading from "@/data/header";
+import { reactive } from "vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  setup() {
+    const data = reactive(sortedArray);
+    const head = reactive(heading);
+
+    return {
+      data,
+      head,
+    };
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+td {
+  border: 1px solid lightgrey;
 }
 </style>
---
+++ src/data/header.js
@@ -0,0 +1,24 @@
+const heading = [
+   "№",
+   "OTM nomi",
+   "Talim shakli",
+   "Talim yonalishi",
+   "Talim tili",
+   "FIO",
+   "Jinsi",
+   "ID Raqami",
+   "Kitob Raqami",
+   "Fan1",
+   "To‘g‘ri javoblar soni",
+   "Ball",
+   "Fan2",
+   "To‘g‘ri javoblar soni",
+   "Ball",
+   "Fan3",
+   "To‘g‘ri javoblar soni",
+   "Ball",
+   "Umumiy Ball",
+   "Sana",
+];
+
+export default heading;
